131903ee7bc72ab66246533a61b601f90241d2d6
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-05 20:00:35 +0530

    initial code

---
+++ findmax.js
@@ -0,0 +1,10 @@
+const findmax=(arr)=>{
+  if(arr.length===0){
+    return null;
+  }
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+  }
+}
+console.log(findmax(arr[12,34,45,65,776,7,8,65,45,34]));

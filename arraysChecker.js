7fae633742862ed168d7ddab862898a8f42841e2
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-07 15:29:23 +0530

    finalsolution

--- arraysChecker.js
+++ arraysChecker.js
@@ -3,14 +3,14 @@ const arraysAreEqual=(arr1,arr2)=>{
   // :value===val));
   // return arr.every((value,index)=>arr.slice(0,index).concat(arr.slice(index+1
   //   )).includes(value));
-  if(arr1.length===arr2.length){
+  if(arr1.length===0||arr2.length===0){
     return false;
   };
 
-    
+   return arr1.every((curVal,index)=>curVal==arr2[index])
 
 }
 console.log(arraysAreEqual([1,2,3,4],[1,2,3,4]));
-console.log(arraysAreEqual([1,2,3,4],[1,2,3,4]));
-console.log(arraysAreEqual([1,2,3,4],[1,2,3,4]));
+console.log(arraysAreEqual([1,6,3,4],[1,2,3,4]));
+console.log(arraysAreEqual([1,2,3,4,68,7],[1,2,3,4]));
 console.log(arraysAreEqual([],[]));

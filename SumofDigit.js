d0ba3c1aa9d5a960af1037fa6f20599e2e1345d1
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-16 13:14:00 +0530

    minorchange

--- SumofDigit.js
+++ SumofDigit.js
@@ -1,12 +1,12 @@
 
 
 // USE Reduce method  and converting into array 
-// const sum=(num)=>{
-// let arr=Array.from(String(num),Number)
-// console.log(arr);
-// return arr.reduce((accum,curElem)=>accum=accum+curElem,0)
-// };
-// console.log(sum(12323));
+const sum=(num)=>{
+let arr=Array.from(String(num),Number)
+console.log(arr);
+return arr.reduce((accum,curElem)=>accum=accum+curElem,0)
+};
+console.log(sum(12323));
 
 
 // sum of digit 

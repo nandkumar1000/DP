f38c53a4b6779f95351afefbee165f2af483222b
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-16 13:05:28 +0530

    BY THE USE OF REDUCE

--- SumofDigit.js
+++ SumofDigit.js
@@ -1,15 +1,7 @@
-const sumofdigit=(num)=>{
-  if(num.length===0){
-    return null;
-  };
-  let sum=0;
-  let count=0;
-  for(let i=0;i<num.length;i++){
-    num/10;
-    count++
-    }
-    console.log(count);
-}
-console.log(sumofdigit(1233454));
-console.log(sumofdigit(-12233));
-console.log(sumofdigit());
+
+const sum=(num)=>{
+let arr=Array.from(String(num),Number)
+console.log(arr);
+return arr.reduce((accum,curElem)=>accum=accum+curElem,0)
+};
+console.log(sum(123231));

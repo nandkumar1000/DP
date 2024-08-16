b04a82984f88c9045ef1514fc122f5b50ba9bbc9
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-16 19:10:17 +0530

    initial solution of isPowerOfTwo

---
+++ Power_of_Two.js
@@ -0,0 +1,13 @@
+
+const isPowerOfTwo =(num)=>{
+  if(num<2){
+    return not_possible;
+  }
+ num=num.math.pow();
+}
+
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(-8));
+console.log(isPowerOfTwo(7));
+console.log(isPowerOfTwo(0));
+console.log(isPowerOfTwo());

8c4f8a4485b93ed7b9440ed8f6db32d3fc84eb0b
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-18 11:15:24 +0530

    using for loop

--- Power_of_Two.js
+++ Power_of_Two.js
@@ -4,9 +4,8 @@ const isPowerOfTwo = (num) => {
   if (num < 1||num === undefined) {
     return false;
   }
-  // Bitwise Operation: The function uses the bitwise AND operation (num & (num - 1)) === 0 to check if num is a power of two. This works because powers of two in binary representation have only one bit set to 1 (e.g., 8 is 1000 in binary). Subtracting 1 from a power of two flips all bits after the least significant 1 to 1, making the AND operation result in 0.
-
-
+ 
+// bitwise operation for checking 
   return (num & (num - 1)) === 0;
 };
 
@@ -16,3 +15,20 @@ console.log(isPowerOfTwo(7));
 console.log(isPowerOfTwo(-7)); 
 console.log(isPowerOfTwo(0));  
 console.log(isPowerOfTwo());  
+
+//another method: without using bitwise operation using for loop
+
+
+function isPower(num) {
+  if (num < 1 || num === undefined) {
+    return false;
+    }
+    for (let i = 0; i < num; i++) {
+      if (num === 2 ** i) {
+        return true;
+        }
+        }
+        return false;
+        }
+        console.log(isPower(23));  
+        console.log(isPower(16));

a18736943c64b96e45981c5df650d2da330821ad
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-18 10:41:28 +0530

    final solution using bitwise operation

--- Power_of_Two.js
+++ Power_of_Two.js
@@ -1,13 +1,18 @@
 
-const isPowerOfTwo =(num)=>{
-  if(num<2){
-    return not_possible;
-  }
- num=num.math.pow();
-}
 
-console.log(isPowerOfTwo(8));
-console.log(isPowerOfTwo(-8));
-console.log(isPowerOfTwo(7));
-console.log(isPowerOfTwo(0));
-console.log(isPowerOfTwo());
+const isPowerOfTwo = (num) => {
+  if (num < 1||num === undefined) {
+    return false;
+  }
+  // Bitwise Operation: The function uses the bitwise AND operation (num & (num - 1)) === 0 to check if num is a power of two. This works because powers of two in binary representation have only one bit set to 1 (e.g., 8 is 1000 in binary). Subtracting 1 from a power of two flips all bits after the least significant 1 to 1, making the AND operation result in 0.
+
+
+  return (num & (num - 1)) === 0;
+};
+
+console.log(isPowerOfTwo(8));  
+console.log(isPowerOfTwo(-8)); 
+console.log(isPowerOfTwo(7));  
+console.log(isPowerOfTwo(-7)); 
+console.log(isPowerOfTwo(0));  
+console.log(isPowerOfTwo());  

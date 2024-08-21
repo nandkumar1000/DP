ea5746318da3e3f7d7fbf61306df6cbfa87e7e60
Nand kumar sahu <shivanandbansal9520@gmail.com>
2024-08-21 11:50:02 +0530

    finalsolution

--- CamelCase_converter.js
+++ CamelCase_converter.js
@@ -1,7 +1,22 @@
-const CamelCase=(str)=>{
-  if(str.length===0||str===undefined){
+
+
+const CamelCase = (str) => {
+  if (!str || str.length === 0) {
     return false;
   }
-}
 
-console.log(CamelCase("HI i aM nAND KUMAR sahu"))
+  let camelCaseStr = str
+    .trim()
+    .split(" ")
+    .map((curElem, index) => {
+      if (index === 0) {
+        return curElem.toLowerCase();
+      } else {
+        return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase();
+      }
+    })
+    .join("");
+  return camelCaseStr.charAt(0).toUpperCase() + camelCaseStr.slice(1);
+};
+
+console.log(CamelCase("HI i aM nAND KUMAR sahu"));
